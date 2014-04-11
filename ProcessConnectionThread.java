e0cafbba8ec1caf1de6437695e2f96b6ec59dfd8
darglk <darglk@gmail.com>
2014-04-11 19:19:53 +0200

    Update ProcessConnectionThread.java

--- ProcessConnectionThread.java
+++ ProcessConnectionThread.java
@@ -1,10 +1,10 @@
 import java.awt.Robot;
-
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-
 import java.io.InputStream;
 
  
+
 
 import javax.microedition.io.StreamConnection;
 
@@ -20,12 +20,18 @@ public class ProcessConnectionThread implements Runnable{
 
     // Constant that indicate command from devices
 
-    private static final int EXIT_CMD = -1;
-
-    private static final int KEY_RIGHT = 1;
-
-    private static final int KEY_LEFT = 2;
-
+    private static final int EXIT_CMD=-1;
+	private static final int REQUEST_ENABLE_BT=1;
+	private static final int MOUSE_LEFT = 2;
+	private static final int MOUSE_RIGHT=3;
+	private static final int MOUSE_MOVE=4;
+	private static final int KEY_LEFT=5;
+	private static final int KEY_RIGHT=6;
+	private static final int SCROLL_ON=7;
+	private static final int SCROLL_OFF=8;
+	private static final int LEFT_PRESS=9;
+	private static final int LEFT_RELEASE=10;
+    private InputStream inputStream;
      
 
     public ProcessConnectionThread(StreamConnection connection)
@@ -46,7 +52,7 @@ public class ProcessConnectionThread implements Runnable{
 
            // prepare to receive data
 
-            InputStream inputStream = mConnection.openInputStream();
+            inputStream = mConnection.openInputStream();
 
              
 
@@ -100,22 +106,39 @@ public class ProcessConnectionThread implements Runnable{
 
             switch (command) {
 
-                case KEY_RIGHT:
-
-                    robot.keyPress(KeyEvent.VK_RIGHT);
-
-                    System.out.println("Right");
-
-                    break;
-
-                case KEY_LEFT:
-
-                    robot.keyPress(KeyEvent.VK_LEFT);
-
-                    System.out.println("Left");
-
-                    break;
-
+            case MOUSE_LEFT:
+            		robot.mousePress(InputEvent.BUTTON1_MASK);
+            		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            	break;
+            case MOUSE_RIGHT:
+            	robot.mousePress(InputEvent.BUTTON3_MASK);
+        		robot.mouseRelease(InputEvent.BUTTON3_MASK);
+            	break;
+            case MOUSE_MOVE:
+            	int x=6*inputStream.read();
+            	int y=4*inputStream.read();
+            	robot.mouseMove(x, y);
+            	break;
+            case KEY_LEFT:
+            	robot.keyPress(KeyEvent.VK_LEFT);
+            	robot.keyRelease(KeyEvent.VK_LEFT);
+            	break;
+            case KEY_RIGHT:
+            	robot.keyPress(KeyEvent.VK_RIGHT);
+            	robot.keyRelease(KeyEvent.VK_RIGHT);
+            	break;
+            case SCROLL_ON:
+            	robot.mousePress(InputEvent.BUTTON2_MASK);
+            	break;
+            case SCROLL_OFF:
+            	robot.mouseRelease(InputEvent.BUTTON2_MASK);
+            	break;
+            case LEFT_PRESS:
+            	robot.mousePress(InputEvent.BUTTON1_MASK);
+            	break;
+            case LEFT_RELEASE:
+            	robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            	break;
            }
 
        } catch (Exception e) {
